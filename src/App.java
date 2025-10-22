06b621174b04d09d082417fce2aa1f20eb5e9196
TaraVar <tara.vartio@gmail.com>
2025-10-22 09:12:19 +0300

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,37 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+       //Kirjoitetaan koodit:
+       String tekija = "Ihminen";
+
+        int luku1 = 0;
+        int luku2 = 0;
+        int tulo = luku1 + luku2;
+        luku1 = 5;
+        luku2 = 2;
+        
+        tulo= luku1 +luku2;
+    
+        System.out.println("Hei olen Tulostin-ohjelma");
+        // System.out.print("Ohjelman tekijä: ");
+        // System.out.println(tekija);
+        System.out.println("Ohjelman tekijä: " + tekija);
+      
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+        System.out.println(luku1 + " + " + luku2 + " = " + tulo); 
+    
+
+
+
+
+
+
+
+
+
+        
+
+
+    }
+}
